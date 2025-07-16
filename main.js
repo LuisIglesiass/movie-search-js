51f3c179baacdd51de350fc112ded98e09ee6c54
Luis Iglesias <luis.iglesias@kuehne-nagel.com>
2025-07-16 10:30:16 +0200

    Fix: Resolve merge conflicts and clean up duplicated code in CSS, HTML, and JS files

---
+++ main.js
@@ -0,0 +1,38 @@
+import apiKey from './apiKey.js';
+
+let searchButton = document.getElementById('search-button');
+let inputUser = document.getElementById('input-user');
+let loadingSpinner = document.getElementById('loading-spinner');
+let mainSection = document.getElementById('main-section');
+
+function loading(ms) {
+	return new Promise(resolve => {
+		loadingSpinner.className = 'loadingSpinner';
+		setTimeout(resolve, ms);
+	});
+}
+
+searchButton.addEventListener('click', async function (event){
+	event.preventDefault();
+	const response = await fetch(`https://www.omdbapi.com/?s=${inputUser.value}&apikey=${apiKey}`)
+    const data = await response.json();
+	mainSection.innerHTML = '';
+
+	if (data.Search) {
+		data.Search.forEach(movie => {
+			const article = document.createElement('article');
+			const image = document.createElement('img');
+			const title = document.createElement('h3');
+
+			image.src = movie.Poster !== "N/A" ? movie.Poster : "";
+			image.alt = movie.Title;
+			title.textContent = movie.Title;
+
+			article.appendChild(image);
+			article.appendChild(title);
+			mainSection.appendChild(article);
+		});
+	} else {
+		mainSection.textContent = 'No se encontraron resultados.';
+	}
+});
