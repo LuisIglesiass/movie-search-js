97b28972b6f22facfdb1e0f3e6d9ebe729d4c9a8
Luis Iglesias <luis.iglesias@kuehne-nagel.com>
2025-07-21 20:16:46 +0200

    new loader while fetching data

--- main.js
+++ main.js
@@ -2,37 +2,47 @@ import apiKey from './apiKey.js';
 
 let searchButton = document.getElementById('search-button');
 let inputUser = document.getElementById('input-user');
-let loadingSpinner = document.getElementById('loading-spinner');
+let loading = document.getElementById('loading');
 let mainSection = document.getElementById('main-section');
 
-function loading(ms) {
-	return new Promise(resolve => {
-		loadingSpinner.className = 'loadingSpinner';
-		setTimeout(resolve, ms);
-	});
+function showLoader() {
+	loading.className = 'loading-animation';
 }
 
-searchButton.addEventListener('click', async function (event){
+function hideLoader() {
+	loading.className = '';
+}
+
+searchButton.addEventListener('click', async function (event) {
 	event.preventDefault();
-	const response = await fetch(`https://www.omdbapi.com/?s=${inputUser.value}&apikey=${apiKey}`)
-    const data = await response.json();
+	showLoader();
 	mainSection.innerHTML = '';
+	try {
+		const response = await fetch(`https://www.omdbapi.com/?s=${inputUser.value}&apikey=${apiKey}`);
+		const data = await response.json();
+		setTimeout(() => {
+			hideLoader();
+			if (data.Search) {
+				data.Search.forEach(movie => {
+					const article = document.createElement('article');
+					const image = document.createElement('img');
+					const title = document.createElement('h3');
 
-	if (data.Search) {
-		data.Search.forEach(movie => {
-			const article = document.createElement('article');
-			const image = document.createElement('img');
-			const title = document.createElement('h3');
+					image.src = movie.Poster !== "N/A" ? movie.Poster : "";
+					image.alt = movie.Title;
+					title.textContent = movie.Title;
 
-			image.src = movie.Poster !== "N/A" ? movie.Poster : "";
-			image.alt = movie.Title;
-			title.textContent = movie.Title;
-
-			article.appendChild(image);
-			article.appendChild(title);
-			mainSection.appendChild(article);
-		});
-	} else {
-		mainSection.textContent = 'No results found.';
+					article.appendChild(image);
+					article.appendChild(title);
+					mainSection.appendChild(article);
+				});
+			} else {
+				mainSection.textContent = 'No results found.';
+			}
+		}, 800);
+	} catch {
+		hideLoader();
+		alert('Something went wrong.');
+		mainSection.innerText = 'Something went wrong';
 	}
 });
